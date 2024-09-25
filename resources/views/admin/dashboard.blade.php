b871279fabae4513ec47a2784622e6bd2fe49b09
yanti-hub <tutikhidayanti2004@gmail.com>
2024-09-25 14:01:35 +0700

    yanti

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SIJASA BANYUWANGI</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  
+  <body>
+    <!-- Navbar -->
+    <nav class="navbar bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand text-white fw-bolder" href="#">
+          <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top ">
+            SIJASA BANYUWANGI
+        </a>
+          <ul class="nav justify-content-end">
+            <li class="nav-item">
+              <a class="nav-link active text-white" aria-current="page" href="#">Beranda</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Kategori</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Kontak</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Paduan</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" style="color: orange;" href="#">Login</a>
+            </li>
+          </ul>
+      </div>
+    </nav>
+
+
+   
+    <div class="container">
+      <img src="{{ asset('assets/gambar/stadiun.jpg') }}"  class="img-fluid" alt="Stadiun Banyuwangi">
+    </div>
+    
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
+
